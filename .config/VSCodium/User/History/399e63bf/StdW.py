d251f8ef98aa049191db29cd649c0687a12c9c8f
Alex <alexbcreiner@gmail.com>
2024-06-11 16:32:15 -0400

    2024-06-11

---
+++ .config/VSCodium/User/History/399e63bf/StdW.py
@@ -0,0 +1,41 @@
+\documentclass{beamer}
+
+\usepackage{fancyvrb}
+\makeatletter
+\def\KV@FV@lastline@default{%
+  \let\FancyVerbStopNum\m@ne
+  \let\FancyVerbStopString\relax}
+\fvset{lastline}
+\makeatother
+\usepackage{listings}
+\usepackage{minted}
+\usepackage{tikz}
+\usepackage{graphicx}
+\usepackage{bm}
+\hypersetup{
+    colorlinks=true,
+    linkcolor=blue,
+    filecolor=magenta,      
+    urlcolor=cyan,
+    pdftitle={Overleaf Example},
+    pdfpagemode=FullScreen,
+    }
+
+\urlstyle{same}
+\usetheme{Madrid}
+
+\title{Complexity Theory and NP-Completeness}
+\author{Alex Creiner}
+
+\institute{Boston College}
+\date{Spring 2024}
+
+\begin{document}
+
+\titlepage
+\begin{frame}{Introduction}
+    \begin{itemize}
+    
+\end{frame}
+
+\end{document}
