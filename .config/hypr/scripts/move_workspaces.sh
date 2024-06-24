d2e91bc50f4724a25f3a5cfeeef4a44c16f6ce6f
Alex Creiner <alexbcreiner@gmail.com>
2024-06-24 02:57:43 -0400

    2024-06-24

---
+++ .config/hypr/scripts/move_workspaces.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+direc=$1
+current=$(hyprctl activeworkspace | grep workspace | awk '{print $3}')
+if [[ $direc == '--left' ]]; then
+    new=$((current-1))
+else
+    new=$((current+1))
+fi
+
+MONITOR="$(hyprctl activeworkspace | grep monitorID | awk '{print $2}')"
+if [[ $MONITOR == '1' && $new -ge 1 && $new -le 5 ]] || [[ $MONITOR == '0' && $new -ge 6 && $new -le 10 ]]; then 
+    hyprctl dispatch movetoworkspace $new
+fi
