55bdb3e2868defca705c20f20ff16a31d1106493
Alex Creiner <alexbcreiner@gmail.com>
2024-07-19 21:21:30 -0400

    2024-07-19

--- .config/nvim/lua/alex/plugins/treesitter.lua
+++ .config/nvim/lua/alex/plugins/treesitter.lua
@@ -1,25 +1,23 @@
-return {}
-
--- return {
---     {
---         "nvim-treesitter/nvim-treesitter",
---         dependencies = { "windwp/nvim-ts-autotag" },
---         config = function()
---             -- Pull just the configs the table, and then from that run the setup function
---             require("nvim-treesitter.configs").setup({
---                 ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "markdown", "markdown_inline"},
---                 sync_install = false,
---                 -- auto_install = true,
---                 highlight = {
---                     enable = true,
---                     disable = {"latex"}
---                 },
---                 indent = { enable = true },
---                 autotag = { enable = true },
---             })
---         end,
---     },
---     {
---         "nvim-treesitter/nvim-treesitter-textobjects",
---     },
--- }
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = { "windwp/nvim-ts-autotag" },
+        config = function()
+            -- Pull just the configs the table, and then from that run the setup function
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "markdown", "markdown_inline"},
+                sync_install = false,
+                -- auto_install = true,
+                highlight = {
+                    enable = true,
+                    disable = {"latex"}
+                },
+                indent = { enable = true },
+                autotag = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+}
